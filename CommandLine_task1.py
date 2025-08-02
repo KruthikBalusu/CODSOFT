5882b71be3fe672fce8a4495e4cb2e82535a7663
KruthikBalusu <kruthikbalusu@gmail.com>
2025-08-02 22:09:46 +0530

    Update CommandLine_task1.py

--- CommandLine_task1.py
+++ CommandLine_task1.py
@@ -1,30 +1,31 @@
-n = input("enter your task 1 add 2.view 3.delete 4.exit : ")
-task = []
-res = []
-k, h = [], []
+print("Task you have on todo list 1 add 2.view 3.delete 4.restore,5.exit ")
+task = [] #All the task will be stored here 
+res = []# res is used to restore the deleted ones
+k, h = [], []# Temporary lists to assist with restoring deleted tasks
 
-while True:
-    ch = input("1.add 2.view 3.delete 4 restore 5.exit: ")
-    if ch == '1':
-        add = input("ENTER YOUR TASK: ")
-        task.append(add)
+while True:# It runs till the condition is False when ch==5 
+    ch = input("1.add 2.view 3.delete 4 restore 5.exit: ")# It is the input statement Where we have to choose the Numbers from 1 to 5
+    if ch == '1':# If we choose 1 as the input we can add the task in our todo list
+        add = input("ENTER YOUR TASK: ")#This is the input for our task
+        task.append(add)#THIS is stored in task list because multiple tasks can be added
         print("TASKED IS ADDED!!")
     elif ch == '2':
-        print(task)
+        print(task)# If it is 2 It will show all the task 
     elif ch == '3':
-        o = int(input("ENTER WHICH TASK U WANT TO DELETE: "))
-        k = task.pop(o)
-        res.append(k)
+        o = int(input("ENTER WHICH TASK U WANT TO DELETE: "))# if we choose 3 it is used to delete the elements o is the imput to delete the elements
+        k = task.pop(o)# In the task list the input number of o will be popped out of the list and stored in k variable
+        res.append(k)# Store the deleted task in res list for possible restore later
         print("TASK HAS BEEN DELETED !!")
     elif ch == '4':
-        h = res.pop()
-        task.append(h)
+        h = res.pop()# res will pop the last values and that are stored in h variable
+        task.append(h)#  Add the restored task back to the main task list
         print("THE RESTORE!! ", h)
 
 
     elif ch == '5':
-        print("EXITED!!")
+        print("EXITED!!")# If user selects 5, show exit message and break the loop
         break
 
     else:
-        print("INVALID ERROR")
+
+        print("INVALID ERROR") #If user enters an invalid choice, show error message
